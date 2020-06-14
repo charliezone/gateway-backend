8d3377d49f46b7f5d20d4a1b3bc162d172f947e5
Carlos Rafael Gonzalez Perdomo <carlitos051186@gmail.com>
2020-06-14 17:47:43 -0400

    complete test cases

--- test/app.test.js
+++ test/app.test.js
@@ -6,6 +6,9 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe("Testing enpoints", () => {
+    let currentAddedGatewayId = null;
+    let currentAddedPeripheralId = null; 
+
     it('Add Gateway', done => {
         chai
             .request(app)
@@ -15,6 +18,7 @@ describe("Testing enpoints", () => {
                 expect(res).to.have.status(200)
                 expect(res.body.data.name).to.equals("gateway 6")
                 expect(res.body.data.ip).to.equals("127.0.0.6")
+                currentAddedGatewayId = res.body.data._id
                 done()
             })
     })
@@ -39,6 +43,58 @@ describe("Testing enpoints", () => {
                 expect(res).to.have.status(200)
                 expect(res.body.data).to.be.a("array")
                 expect(res.body.data).to.have.lengthOf.at.least(1)
+                done()
+            })
+    })
+
+    it('Add peripheral', done => {
+        chai
+            .request(app)
+            .post('/api/add/peripheral')
+            .send({"id": `${currentAddedGatewayId}`, "vendor": "Peripheral 1", "status": "online"})
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body.data.vendor).to.equals("Peripheral 1")
+                expect(res.body.data.status).to.equals("online")
+                currentAddedPeripheralId = res.body.data._id
+                done()
+            })
+    })
+
+    it('Get Peripherals at least the test inserted one', done => {
+        chai
+            .request(app)
+            .get('/api/peripherals')
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body.data).to.be.a("array")
+                expect(res.body.data.map(e => e._id)).to.include(currentAddedPeripheralId)
+                expect(res.body.data).to.have.lengthOf.at.least(1)
+                done()
+            })
+    })
+
+    it('Delete previus created peripheral', done => {
+        chai
+            .request(app)
+            .delete(`/api/remove/peripheral/${currentAddedPeripheralId}`)
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body.success).to.equals(true)
+                expect(res.body.data._id).to.equals(currentAddedPeripheralId)
+                done()
+            })
+    })
+    
+
+    it('Delete previus created gateway', done => {
+        chai
+            .request(app)
+            .delete(`/api/remove/gateway/${currentAddedGatewayId}`)
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body.success).to.equals(true)
+                expect(res.body.data._id).to.equals(currentAddedGatewayId)
                 done()
             })
     })
