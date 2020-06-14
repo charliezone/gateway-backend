03a0b7bccfa023755fae410eb6e6db1252b97752
Carlos Rafael Gonzalez Perdomo <carlitos051186@gmail.com>
2020-06-14 16:02:24 -0400

    add cors and use it

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require('cors')
 const mongoose = require("mongoose")
 const routes = require("./routes")
 const bodyParser = require("body-parser")
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 
 mongoose
   .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
